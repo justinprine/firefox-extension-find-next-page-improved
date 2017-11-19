37d5cae053d0a38cbf84e643fdb47c7b83045d1d
Artemy Tregubenko <me@arty.name>
2017-11-19 22:32:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Find next page
+
+## a Firefox extension
+
+This extension allows you to conveniently navigate to the next page when pressing Space at the end of the page
+
+## Installation 
+
+[Find next page](https://addons.mozilla.org/de/firefox/addon/find-next-page/)
+
+## License 
+
+This code is licensed under [Mozilla Public License version 2](https://www.mozilla.org/en-US/MPL/2.0/)
---
+++ index.js
@@ -0,0 +1,43 @@
+function isScrollingSpaceEvent(event) {
+  const keyIsSpace = (event.key === ' ');
+  const eventComesFromInput = (event.target.nodeName.match(/input|textarea|select/i) || event.target.hasAttribute('contenteditable'));
+  return (keyIsSpace && !eventComesFromInput);
+}
+
+function cantScrollFurther(scrollingElement) {
+  const scrollHeight = scrollingElement.scrollHeight;
+  const viewportHeight = scrollingElement.clientHeight;
+  const lowestVisibleOffset = scrollingElement.scrollTop + viewportHeight;
+  
+  const pageIsScrollable = (viewportHeight < scrollHeight);
+  const cantScrollFurther = (lowestVisibleOffset === scrollHeight);
+  return (pageIsScrollable && cantScrollFurther);
+}
+
+function getNextHRefFromLink(link) {
+  const linkIsNext = (link.rel === 'next');
+  const linkHasHRef = !!link.href;
+  if (linkIsNext && linkHasHRef) {
+    return link.href;
+  }
+  return null;
+}
+
+function getNextHRefFromDocument(document) {
+  const links = [...document.getElementsByTagName('link'), ...document.links];
+  const nextHRefs = links.map(getNextHRefFromLink).filter(Boolean);
+  return nextHRefs[0];
+}
+
+function goToNextHRef() {
+  const nextHRef = getNextHRefFromDocument(document);
+  if (nextHRef) {
+    window.location = nextHRef;
+  }
+}
+
+document.addEventListener('keypress', function(event) {
+  if (isScrollingSpaceEvent(event) && cantScrollFurther(document.scrollingElement)) {
+    goToNextHRef();
+  }
+}, false);
